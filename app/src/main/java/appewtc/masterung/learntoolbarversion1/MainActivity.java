777f2c08f65bb182d3b6623a9ea499f6a906dd88
masterUNG <phrombutr@gmail.com>
2016-10-14 00:23:11 +0700

    Step 1 OK

---
+++ app/src/main/java/appewtc/masterung/learntoolbarversion1/MainActivity.java
@@ -0,0 +1,31 @@
+package appewtc.masterung.learntoolbarversion1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        //Set Text
+        getSupportActionBar().setTitle(R.string.my_title);
+        getSupportActionBar().setSubtitle(R.string.sub_title);
+
+        //Set Icon
+        //getSupportActionBar().setIcon();
+
+
+    }   // Main Method
+
+}   // Main Class
